cfedc663b394c126bd15deaa46eeb5794cf9a85f
anoobissac <anoop.anoob@gmail.com>
2022-05-30 17:06:50 +0530

    Update settings.py

--- ecart/settings.py
+++ ecart/settings.py
@@ -149,7 +149,7 @@ LOGOUT_REDIRECT_URL = 'home'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
 EMAIL_HOST_USER='anoob.elearningapp@gmail.com'
-EMAIL_HOST_PASSWORD='godislove123456'
+EMAIL_HOST_PASSWORD='ZAwe@2019'
 EMAIL_USE_TLS=True
 EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 
